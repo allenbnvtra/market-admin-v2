51fa32f22d628b95454238971e027b511a5648aa
Allen Buenaventura <115561893+allenbnvtra@users.noreply.github.com>
2024-07-20 20:52:43 +0800

    d

--- components/ui/MobileNav/MobileNav.tsx
+++ components/ui/MobileNav/MobileNav.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link'
 
 const MobileNav = () => {
   return (
-    <nav className="left-0 right-0 z-[800] flex h-[5rem] w-full items-center justify-between border-t-2 border-t-gray-100 bg-white shadow-md md:hidden">
+    <nav className="left-0 right-0 z-[800] flex h-[4.5rem] w-full items-center justify-between border-t-2 border-t-gray-100 bg-white px-6 shadow-md md:hidden">
       {SidebarItems.map(({ icon: Icon, title, path }) => (
-        <div key={title} className="px-3 text-slate-600">
+        <div key={title} className="px-1 text-slate-600">
           {path ? (
-            <Link href={path} className="flex flex-col items-center gap-2">
+            <Link href={path} className="flex flex-col items-center">
               <Icon className="text-2xl" />
               <p className="text-xs">{title}</p>
             </Link>
           ) : (
-            <div className="flex flex-col items-center gap-2">
+            <div className="flex flex-col items-center">
               <Icon className="text-2xl" />
               <p className="text-xs">{title}</p>
             </div>
