d9ffb760c50181e8fe2d9f5eb3cb5212aca5a7b1
Allen Buenaventura <115561893+allenbnvtra@users.noreply.github.com>
2024-07-22 03:13:42 +0800

    some changes in table

--- components/ui/Tenants/Table.tsx
+++ components/ui/Tenants/Table.tsx
@@ -19,9 +19,9 @@ const Table = () => {
 
   return (
     <>
-      <div className="z-0 mt-4 h-[34.2rem] overflow-x-auto rounded-xl border border-[#6a76894c] bg-white shadow-md">
-        <div className="relative h-full overflow-y-auto">
-          <table className="w-full">
+      <div className="relative z-0 mt-4 h-[34.2rem] overflow-x-auto rounded-xl border border-[#6a76894c] bg-white shadow-md">
+        <div className="h-[calc(100%-4rem)] overflow-y-auto rounded-t-xl">
+          <table className="w-full rounded-t-xl">
             <thead className="sticky top-0 text-sm">
               <tr>
                 <th className="t_head w-[5%]">
@@ -74,89 +74,20 @@ const Table = () => {
                   />
                 </td>
               </tr>
-              <tr className="border-b border-b-slate-300">
-                <td className="t_column text-center">
-                  <input
-                    className="h-4 w-4 cursor-pointer"
-                    type="checkbox"
-                    name=""
-                    id="delete"
-                  />
-                </td>
-                <td className="t_column">
-                  7892309182303489623490698731dasdaiuhi
-                </td>
-                <td className="t_column">Allen V. Buenaventura</td>
-                <td className="t_column">hen101</td>
-                <td className="t_column">allenbnvtra</td>
-                <td className="t_column">P 20,832.99</td>
-                <td className="flex items-center justify-center gap-2 py-3 text-lg md:text-[16px]">
-                  <FaRegListAlt
-                    title="View Bills"
-                    className="cursor-pointer text-blue-700"
-                  />
-                  <FaRegEdit
-                    title="Edit"
-                    className="cursor-pointer text-green-600"
-                    onClick={() => setIsEditTenantModalOpen(true)}
-                  />
-                  <FiArchive
-                    title="Archive"
-                    className="cursor-pointer text-red-600"
-                    onClick={() => setIsDeleteModalOpen(true)}
-                  />
-                </td>
-              </tr>
-              <tr className="border-b border-b-slate-300">
-                <td className="t_column text-center">
-                  <input
-                    className="h-4 w-4 cursor-pointer"
-                    type="checkbox"
-                    name=""
-                    id="delete"
-                  />
-                </td>
-                <td className="t_column">
-                  7892309182303489623490698731dasdaiuhi
-                </td>
-                <td className="t_column">Allen V. Buenaventura</td>
-                <td className="t_column">hen101</td>
-                <td className="t_column">allenbnvtra</td>
-                <td className="t_column">P 20,832.99</td>
-                <td className="flex items-center justify-center gap-2 py-3 text-lg md:text-[16px]">
-                  <FaRegListAlt
-                    title="View Bills"
-                    className="cursor-pointer text-blue-700"
-                  />
-                  <FaRegEdit
-                    title="Edit"
-                    className="cursor-pointer text-green-600"
-                    onClick={() => setIsEditTenantModalOpen(true)}
-                  />
-                  <FiArchive
-                    title="Archive"
-                    className="cursor-pointer text-red-600"
-                    onClick={() => setIsDeleteModalOpen(true)}
-                  />
-                </td>
-              </tr>
+              {/* Additional rows as needed */}
             </tbody>
           </table>
-          <div className="absolute bottom-0 z-0 w-full bg-[#344d7aed] text-white">
-            <div>
-              <div>
-                <div className="flex w-full items-center gap-3 border-t px-5 py-4 text-xs">
-                  <p>1-10 of 210</p>
-                  <div className="flex gap-3 text-sm">
-                    <p className="cursor-pointer">
-                      <FaAngleLeft />
-                    </p>
-                    <p className="cursor-pointer">
-                      <FaAngleRight />
-                    </p>
-                  </div>
-                </div>
-              </div>
+        </div>
+        <div className="absolute bottom-0 left-0 w-full rounded-b-xl bg-[#344d7aed] text-white">
+          <div className="flex w-full items-center gap-3 border-t px-5 py-4 text-xs">
+            <p>1-10 of 210</p>
+            <div className="flex gap-3 text-sm">
+              <p className="cursor-pointer">
+                <FaAngleLeft />
+              </p>
+              <p className="cursor-pointer">
+                <FaAngleRight />
+              </p>
             </div>
           </div>
         </div>
